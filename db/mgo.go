b73f61c243a9d403707f2c7f531ac7eb5c48cafd
programmingbunny <bunnyprogramming@gmail.com>
2022-08-19 15:06:23 -0500

    separated book & chapter collections

--- db/mgo.go
+++ db/mgo.go
@@ -11,6 +11,8 @@ import (
 
 var BookCollection *mongo.Collection = configs.GetCollection(configs.DB, "BookDetails")
 
+var ChapterCollection *mongo.Collection = configs.GetCollection(configs.DB, "Chapters")
+
 func InsertBook(ctx context.Context, newBook models.Book) (result *mongo.InsertOneResult, err error) {
 	result, err = BookCollection.InsertOne(ctx, newBook)
 	if err != nil {
@@ -20,7 +22,7 @@ func InsertBook(ctx context.Context, newBook models.Book) (result *mongo.InsertO
 }
 
 func InsertChapter(ctx context.Context, newChapter models.Chapter) (result *mongo.InsertOneResult, err error) {
-	result, err = BookCollection.InsertOne(ctx, newChapter)
+	result, err = ChapterCollection.InsertOne(ctx, newChapter)
 	if err != nil {
 		return nil, err
 	}
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/programmingbunny/epub-backend/configs"
+	routes "github.com/programmingbunny/epub-backend/service"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("initial commit, let's gooo!")
+	router := mux.NewRouter()
 
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(router) //add this
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
