a384df8ddb602e8d01415a1a58e8d7d767f0dbd5
programmingbunny <bunnyprogramming@gmail.com>
2022-09-01 23:56:37 -0500

    added endpoint for retrieving header image data

--- service/service.go
+++ service/service.go
@@ -13,6 +13,7 @@ func Routes(router *mux.Router) {
 	router.HandleFunc("/chapter", chapters.CreateChapter()).Methods("POST")
 	router.HandleFunc("/chapters/{bookId}", chapters.GetAllChapters()).Methods("GET")
 	router.HandleFunc("/chapter/{chapterId}", chapters.GetSingleChapter()).Methods("GET")
-	router.HandleFunc("/createChapterImage", books.CreateChapterHeader()).Methods("POST")
 
+	router.HandleFunc("/getChapterImage/{bookId}/{chNum}", books.GetChapterHeader()).Methods("GET")
+	router.HandleFunc("/createChapterImage", books.CreateChapterHeader()).Methods("POST")
 }
